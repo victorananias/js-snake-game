2b6b0048eef05f28744238af4123346ff589d368
Victor <victor@ananias.dev>
2019-10-25 13:30:28 -0300

    fix SPEEDUP

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = () => {
   const KEY_UP = 87;
   const KEY_DOWN = 83;
   const KEY_PAUSE = 32;
+  const SPEEDUP = 0.8
   
   const SIZE = parseInt(getStyle(document.querySelector('.snake-part')).width);
   
@@ -59,7 +60,7 @@ window.onload = () => {
   function setDirection(e) {
     
     let now = new Date().getTime();
-    if (now - lastClick <= (SPEED * checkout)) {
+    if (now - lastClick <= (SPEED * SPEEDUP)) {
       return;
     }
 
